d3eb32303786613cdfad019813ca3b4d5807ee69
Miliuta Siarhei <vaissedl@gmail.com>
2019-06-07 17:30:33 +0300

    moved methods from game component to service

--- src/app/game-panel/game-panel.component.ts
+++ src/app/game-panel/game-panel.component.ts
@@ -1,26 +1,27 @@
 import { Component, OnInit, Injectable} from '@angular/core';
 import {Cell} from '../cell';
 import {DataExchangeService} from '../data-exchange.service';
-
-export const cells = new Map();
+import {VitalService} from '../vital.service';
 
 @Component({
   selector: 'app-game-panel',
   templateUrl: './game-panel.component.html',
   styleUrls: ['./game-panel.component.css']
 })
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class GamePanelComponent implements OnInit {
   gameSize: number;
 
-  constructor( private dexServise: DataExchangeService) { }
+
+  constructor( private dexServise: DataExchangeService, private vitalService: VitalService) { }
 
   ngOnInit() {
     this.dexServise.currentGameSize.subscribe(gameSize => this.gameSize = gameSize);
-    this.populate();
-    this.setNeighbours();
+    this.vitalService.reboot(this.gameSize);
   }
 
   private numberArray(): any[] {
@@ -28,6 +29,7 @@ export class GamePanelComponent implements OnInit {
   }
 
   toogleCell(event) {
+    const cells = this.vitalService.cells;
     const tableCell = event.currentTarget;
     const cell =  cells.get(tableCell.id);
     if (tableCell.style.backgroundColor === '') {
@@ -48,53 +50,11 @@ export class GamePanelComponent implements OnInit {
     }
   }
 
-  populate(): void {
-    for (let i = 0; i < this.gameSize; i++) {
-      for (let j = 0; j < this.gameSize; j++) {
-        const cell = new Cell(i + ':' + j);
-        cells.set(cell.id, cell);
-      }
-    }
-  }
 
-  setNeighbours(): void {
-    cells.forEach((cell: Cell, key: string) =>  {
-      const id = cell.id;
-      let row = Number(id.split(':')[0]);
-      let column = Number(id.split(':')[1]);
-      let neighbourRow;
-      let neighbourColumn;
-
-      for (let i =  -1; i < 2; i++) {
-        for (let j = -1; j < 2; j++) {
-          neighbourRow = row + i;
-          if (neighbourRow < 0) {
-            neighbourRow = this.gameSize - 1;
-          } else if (neighbourRow > this.gameSize - 1) {
-            neighbourRow = 0;
-          }
-
-          neighbourColumn = column + j;
-          if (neighbourColumn < 0) {
-            neighbourColumn = this.gameSize - 1;
-          } else if (neighbourColumn > this.gameSize - 1) {
-            neighbourColumn = 0;
-          }
-
-          const neighbourID = (neighbourRow) + ':' + (neighbourColumn);
-
-          // situation i === 0 && j === 0 represents current cell, so we shouldn't push it to neighbours collection
-          if (!(i === 0 && j === 0)) {
-            cell.neighbours.set(neighbourID, cells.get(neighbourID));
-          }
-        }
-      }
-    });
-
-  }
 
   refresh(): void {
     console.log('Refresh started');
+    const cells = this.vitalService.cells;
     cells.forEach((cell: Cell, key: string) => {
       const td = document.getElementById(cell.id);
       td.style.backgroundColor = cell.color;
--- src/app/vital.service.ts
+++ src/app/vital.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import {cells} from './game-panel/game-panel.component';
 import {Cell} from './cell';
 
 
@@ -8,22 +7,83 @@ import {Cell} from './cell';
 })
 export class VitalService {
 
+
+  private _cells = new Map();
+
   constructor() { }
+
+  private clean(): void {
+    this._cells.forEach((cell: Cell, key: string) =>  {
+      cell.liveOrDie();
+    });
+  }
+
+  private check(): void {
+    this._cells.forEach((cell: Cell, key: string) =>  {
+      cell.checkNeigboursAlive();
+    });
+  }
 
   run(): void {
     this.check();
     this.clean();
   }
 
-  private clean(): void {
-    cells.forEach((cell: Cell, key: string) =>  {
-      cell.liveOrDie();
+  reboot(gameSize: number): void {
+    this.dropCellsMap();
+    this.populate(gameSize);
+    this.setNeighbours(gameSize);
+  }
+
+  private dropCellsMap(): void {
+    this._cells = new Map();
+  }
+
+  private populate(gameSize: number): void {
+    for (let i = 0; i < gameSize; i++) {
+      for (let j = 0; j < gameSize; j++) {
+        const cell = new Cell(i + ':' + j);
+        this._cells.set(cell.id, cell);
+      }
+    }
+  }
+
+  private setNeighbours(gameSize: number): void {
+    this._cells.forEach((cell: Cell, key: string) =>  {
+      const id = cell.id;
+      let row = Number(id.split(':')[0]);
+      let column = Number(id.split(':')[1]);
+      let neighbourRow;
+      let neighbourColumn;
+
+      for (let i =  -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+          neighbourRow = row + i;
+          if (neighbourRow < 0) {
+            neighbourRow = gameSize - 1;
+          } else if (neighbourRow > gameSize - 1) {
+            neighbourRow = 0;
+          }
+
+          neighbourColumn = column + j;
+          if (neighbourColumn < 0) {
+            neighbourColumn = gameSize - 1;
+          } else if (neighbourColumn > gameSize - 1) {
+            neighbourColumn = 0;
+          }
+
+          const neighbourID = (neighbourRow) + ':' + (neighbourColumn);
+
+          // situation i === 0 && j === 0 represents current cell, so we shouldn't push it to neighbours collection
+          if (!(i === 0 && j === 0)) {
+            cell.neighbours.set(neighbourID, this._cells.get(neighbourID));
+          }
+        }
+      }
     });
   }
 
-  private check(): void {
-    cells.forEach((cell: Cell, key: string) =>  {
-      cell.checkNeigboursAlive();
-    });
+  get cells(): Map {
+    return this._cells;
   }
 }
