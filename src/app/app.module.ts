3010c2bf18399f9eabd1154c2df9dc686e0196e9
Miliuta Siarhei <vaissedl@gmail.com>
2019-06-07 09:55:44 +0300

    second part of bulk commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GamePanelComponent } from './game-panel/game-panel.component';
+import {FormsModule} from '@angular/forms';
+import { ControlPanelComponent } from './control-panel/control-panel.component';
+
+import {ButtonModule} from 'primeng/button';
+import {SliderModule} from 'primeng/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamePanelComponent,
+    ControlPanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ButtonModule,
+    SliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
