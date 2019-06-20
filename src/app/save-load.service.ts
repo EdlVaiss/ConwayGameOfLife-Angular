bd78c9314eb88303ad9ae9d2ef2109a858742df1
Miliuta Siarhei <vaissedl@gmail.com>
2019-06-20 13:29:21 +0300

    handle fileReading with Promise

--- src/app/save-load.service.ts
+++ src/app/save-load.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Cell} from './cell';
 import {saveAs} from 'file-saver/dist/FileSaver';
+import {Promise} from 'es6-promise';
 
 @Injectable({
   providedIn: 'root'
@@ -25,25 +26,32 @@ export class SaveLoadService {
     console.log('Game saved successfully!');
   }
 
-  load(file: File): void {
+ load(file: File): void {
     this._cells = [];
+    this.getFileContent(file).then((content) => {
+    if (typeof  content === 'string') {
+      const buff: Array<string> = JSON.parse(content);
+      buff.forEach((entry) => {
+        this._cells.push(new Map(JSON.parse(entry)));
+        console.log('end of OnFullFilled');
+      });
+
+    }
+    });
+   console.log('end of load()');
+  }
+
+  private getFileContent(file: File): Promise<string> {
     const reader = new FileReader();
+    return new Promise((resolve) => {
+      reader.onerror = () => {
+        reader.abort();
+      };
+      reader.onloadend = () => {
+        resolve(reader.result);
+       };
+      reader.readAsText(file);
+    });
+  }
 
-    reader.onloadend = () => {
-      const content = reader.result;
-
-      if (typeof  content === 'string') {
-        const buff: Array<string> = JSON.parse(content);
-        buff.forEach((entry) => {
-          this._cells.push(new Map(JSON.parse(entry)));
-        });
-
-        console.log(this._cells);
-        console.log('Game loaded');
-      }
-    };
-
-    reader.readAsText(file);
-
-   }
 }
