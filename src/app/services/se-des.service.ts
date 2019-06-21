89a3958ca641839087d93639e2af4f273ee80f70
Miliuta Siarhei <vaissedl@gmail.com>
2019-06-21 18:06:35 +0300

    deserialization in progress

--- src/app/services/se-des.service.ts
+++ src/app/services/se-des.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Game} from '../model/game';
+import {Cell} from '../model/cell';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,44 @@ export class SeDesService {
   constructor() { }
 
   serialize(game: Game): string {
-    return '';
+    /*const buff: Array<string> = [];
+    data.forEach((entry) => {buff.push(JSON.stringify(Array.from(entry.entries()))); });
+
+    const str = JSON.stringify(buff);*/
+    const jsonStr = JSON.stringify(game);
+    return jsonStr;
   }
 
   deserialize(str: string): Game {
-    return new Game(20);
+
+   /* console.log('started parsing');
+    const buff: Array<string> = JSON.parse(content);
+
+    buff.forEach((entry) => {
+      const parsedEntry: Map<string, Cell> =  new Map(JSON.parse(entry));
+      console.log('parsedEntry');
+      console.log(parsedEntry);
+      this._cells.push(new Map(JSON.parse(entry) ));
+      console.log('entry');
+      console.log(entry);
+    });
+    console.log('saveLoadServ cells');
+    console.log(this._cells);*/
+const gameRAW = JSON.parse(str);
+const game = new Game(gameRAW._fieldSize);
+const gameStateCellsMap = new Map<string, Cell>();
+
+   /* gameRAW._history.forEach((gameState) => {
+
+      gameState.forEach((cellsMap) => {
+
+      });
+    });*/
+
+console.log('gfs ' + gameRAW._history[0]._cells[0]._id);
+
+console.log('parsed game');
+console.log(gameRAW);
+    return game;
   }
 }
