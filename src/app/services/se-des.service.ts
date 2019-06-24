e5c8b98c7c2d3c815a1d1b37eed41896055d6d30
Miliuta Siarhei <vaissedl@gmail.com>
2019-06-24 10:23:33 +0300

    deserialize() complete

--- src/app/services/se-des.service.ts
+++ src/app/services/se-des.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Game} from '../model/game';
 import {Cell} from '../model/cell';
+import {GameState} from '../model/game-state';
 
 @Injectable({
   providedIn: 'root'
@@ -10,44 +11,29 @@ export class SeDesService {
   constructor() { }
 
   serialize(game: Game): string {
-    /*const buff: Array<string> = [];
-    data.forEach((entry) => {buff.push(JSON.stringify(Array.from(entry.entries()))); });
-
-    const str = JSON.stringify(buff);*/
     const jsonStr = JSON.stringify(game);
     return jsonStr;
   }
 
   deserialize(str: string): Game {
 
-   /* console.log('started parsing');
-    const buff: Array<string> = JSON.parse(content);
+    const gameRAW = JSON.parse(str);
+    const game = new Game(gameRAW._fieldSize);
+    const gameStateCellsMap = new Map<string, Cell>();
 
-    buff.forEach((entry) => {
-      const parsedEntry: Map<string, Cell> =  new Map(JSON.parse(entry));
-      console.log('parsedEntry');
-      console.log(parsedEntry);
-      this._cells.push(new Map(JSON.parse(entry) ));
-      console.log('entry');
-      console.log(entry);
-    });
-    console.log('saveLoadServ cells');
-    console.log(this._cells);*/
-const gameRAW = JSON.parse(str);
-const game = new Game(gameRAW._fieldSize);
-const gameStateCellsMap = new Map<string, Cell>();
+    gameRAW._history.forEach((gameStateObj) => {
+      const gameState = new GameState();
+      gameStateObj._cells.forEach((cellObj) => {
+        const cell = new Cell(cellObj._id);
+        cell.isAlive = cellObj._isAlive;
+        cell.color = cellObj._color;
+        cell.age = parseInt(cellObj._age);
 
-   /* gameRAW._history.forEach((gameState) => {
-
-      gameState.forEach((cellsMap) => {
-
+        gameState.cells.set(cell.id, cell);
       });
-    });*/
-
-console.log('gfs ' + gameRAW._history[0]._cells[0]._id);
-
-console.log('parsed game');
-console.log(gameRAW);
+      game.history.push(gameState);
+    });
+console.log('Game serialized successfully!')
     return game;
   }
 }
