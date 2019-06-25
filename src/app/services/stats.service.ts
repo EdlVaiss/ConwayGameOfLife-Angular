c6190baa85e5f3a1cff0425877e8e2d1d79579a3
Miliuta Siarhei <vaissedl@gmail.com>
2019-06-25 15:42:12 +0300

    process() and clearCurrentStats() methods complete

--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -7,13 +7,40 @@ import {Cell} from '../model/cell';
 })
 export class StatsService {
 
-private stats: Stats;
+private _stats: Stats;
 
   constructor() {
-    this.stats = new Stats();
+    this._stats = new Stats();
+  }
+
+  clearCurrentStateStats(): void {
+    this._stats.diedLastGameState = 0;
+    this._stats.population = 0;
+    this._stats.bornCurrentGameState = 0;
   }
 
   process(cell: Cell): void {
+    if (cell.isAlive) {
+      if (cell.age === 0) {
+        this._stats.bornAllGame++;
+        this._stats.bornCurrentGameState++;
+      }
 
+      this._stats.population++;
+
+      if (cell.neighboursAlive < 2 || cell.neighboursAlive > 3) {
+        this._stats.diedAllGame++;
+        this._stats.diedLastGameState++;
+      }
+
+      if (cell.age > this._stats.eldestCellAge) {
+        this._stats.eldestCellAge = cell.age;
+      }
+    }
+
+  }
+
+  get stats(): Stats {
+    return this._stats;
   }
 }
